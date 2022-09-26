d4ef3be594faa6f4ea87bb513b211bbab0b86c78
abbygraves <abbygraves14@gmail.com>
2022-09-26 06:47:17 -0500

    completed and styled the contact component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,10 +1,93 @@
-/* WHEN I am presented with the Contact section
-THEN I see a contact form with fields for a name, an email address, and a message */
-/* WHEN I move my cursor out of one of the form fields without entering text
-THEN I receive a notification that this field is required */
-/* WHEN I enter text into the email address field
-THEN I receive a notification if I have entered an invalid email address  */
-
-// INCOMPLETE 
+// DONE 
+// Credit: Lesson 20 Module Content 2U Bootcamp
 
 // CONTACT COMPONENT
+import React, { useState } from "react";
+import { validateEmail } from "../../utils/helpers";
+
+function ContactForm() {
+  const [formState, setFormState] = useState({ name: "", email: "", message: "" });
+  const [errorMessage, setErrorMessage] = useState("");
+  const { name, email, message } = formState;
+
+  // HANDLES FORM SUBMIT BUTTON
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+  }
+
+  // HANDLES CHANGES IN THE FORM INPUTS
+  function handleChange(e) {
+    if (e.target.name === "email") {
+      const isValid = validateEmail(e.target.value);
+      console.log(isValid);
+      if (!isValid) {
+        setErrorMessage("Your email is invalid.");
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage("");
+      }
+    }
+    // console.log("errorMessage", errorMessage);
+
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+  }
+  // console.log(formState);
+
+  // JSX
+  return (
+    <section className="sections contact">
+      <h2>Contact Me</h2>
+      <form id="contact-form" onSubmit={handleSubmit}>
+        {/* NAME INPUT  */}
+        <div>
+          <label htmlFor="name">Name:</label>
+          <br/>
+          <input
+            type="text"
+            name="name"
+            defaultValue={name}
+            onBlur={handleChange}
+          />
+        </div>
+        {/* EMAIL INPUT  */}
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <br/>
+          <input
+            type="email"
+            name="email"
+            defaultValue={email}
+            onBlur={handleChange}
+          />
+        </div>
+        {/* MESSAGE TEXT AREA */}
+        <div>
+          <label htmlFor="message">Message:</label>
+          <br/>
+          <textarea
+            name="message"
+            rows="5"
+            defaultValue={message}
+            onBlur={handleChange}
+          />
+        </div>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  );
+}
+
+export default ContactForm;
